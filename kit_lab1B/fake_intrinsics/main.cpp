b8906a0ea62fb3b2b3df40e3e16790b74fc31b08
WoneBone <nuno.g.tribolet.de.abreu@tecnico.ulisboa.com>
2024-09-17 19:01:30 +0100

    fim da aula

--- kit_lab1B/fake_intrinsics/main.cpp
+++ kit_lab1B/fake_intrinsics/main.cpp
@@ -1,40 +1,50 @@
 #include "fake_intrinsics.h"
 
+void clampedExp(float* values, int* exponents, float* output, int N){
+
+    for (int i=0; i<N; i+=VECTOR_LENGTH){
+
+		__vfloat x = _vload(&values[i]);
+		__vint exp = _vload(&exponents[i]);
+		__vfloat c = _vbcast(1.0f);
+		__vint sub =  _vbcast(-1);
+		__vint zero = _vbcast(0);
+		__vbool mask = _vgt(exp, zero);
+		
+		//exponent
+		while(_vpopcnt(mask) > 1){
+			//multiply those whose exponent is greater then 1
+			c = _vmul(c, x, mask);
+			exp = _vadd(exp, sub);
+			mask = _vgt(exp, zero);
+		}
+		
+		__vfloat nine = _vbcast(9.999f);
+		mask = _vgt(c, nine);
+		c = _vcopy(c, nine, mask);
+
+		_vstore(&output[i], c);
+
+	}
+}
+
+
+
 int main() 
 {
-    //Input for Problem 1 | N = 80
-    float in[N] = {-1,-1,-1,-1,-1,-1,-1,-1,
-                    -1,-1,1,1,-1,-1,1,1,
-                    -1,-1,1,1,-1,-1,1,1,
-                    -1,-1,1,1,-1,-1,1,1,
-                    -1,-1,1,1,-1,-1,1,1,
-                    -1,-1,1,1,-1,-1,1,1,
-                    -1,-1,1,1,-1,-1,1,1,
-                    -1,-1,1,1,-1,-1,1,1,
-                    -1,-1,1,1,-1,-1,1,1,
-                    1,1,1,1,1,1,1,1};
-    
-    //Inputs for Problem 2 | N = 16
-    //float in[N] = {3.0,2.0,2.5,1.25,5.5,0.5,10.1,3.15,         
-    //                  1.75,6.55,1.63,1.5,4.33,0.15,1.95,2.83};
+	float values[] = {3.0, 2.0, 2.5, 1.25, 5.5, 0.5, 10.1, 3.15, 1.75, 6.55, 1.63, 1.5, 4.33, 0.15, 1.95, 2.83};
+	int exponents[] = {0, 2, 3, 10, 0, 4, 0, 3, 5, 1, 4, 9, 0, 5, 0, 3};
+	float output[16] = {0};
 
-    //int exponents[N] = {0, 2, 3, 10, 0, 4, 0, 3,                 
-                        //5, 1, 4, 9, 0, 5, 0, 3};
+	clampedExp(values, exponents, output, N);
 
-    float out[N] = {0};
-
-    for (int i=0; i<N; i+=VECTOR_LENGTH)
-    {
-        __vfloat x = _vload(&in[i]); 
-        __vfloat zeros = _vbcast(0.f);
-        __vbool mask = _vlt(x,zeros);
-        __vfloat y = _vsub(zeros,x,mask);
-        mask = _vnot(mask);
-        y = _vcopy(y,x,mask);
-        _vstore(&out[i],y);
-    }
-
+	for(int i = 0; i < 16; i++){
+		printf("%f  ", output[i]);
+	}
+	printf("\n");
     printStats();
+
+
     
     return 0;
 }

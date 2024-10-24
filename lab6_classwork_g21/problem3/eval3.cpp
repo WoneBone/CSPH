a5d1f654aab2e38b357b4ab0d4eb78a8f6e4347b
Ares-Santos <alex.ares.santos@gmail.com>
2024-10-24 18:07:25 +0100

    3

--- lab6_classwork_g21/problem3/eval3.cpp
+++ lab6_classwork_g21/problem3/eval3.cpp
@@ -36,12 +36,12 @@ void syclDistance(sycl::queue Queue, int** data, int* array, float** dist, float
     sycl::event event;
      event = Queue.submit([&](sycl::handler& h){
         h.parallel_for(sycl::range<1>(N), [=](sycl::id<1> i){
-            for(int i=0 ; i<N ; i++){
+/*             for(int i=0 ; i<N ; i++){
                 for(int j=0; j<N; j++ ){
                     if(data[i][j]==array[j])
                         dist[i][j]=array[j]*3;
                 }
-            }
+            } */
         });
        
     });
